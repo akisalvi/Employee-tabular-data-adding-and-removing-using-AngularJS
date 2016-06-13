366817a44ce49f78db5527f34f04f1811645b90c
akisalvi <salviakshay262626@gmail.com>
2016-06-13 12:03:37 +0000

    Add files via upload

---
+++ app/scripts/controllers/adddataController.js
@@ -0,0 +1,34 @@
+'use strict';
+
+
+var app = angular.module('mytableApp');
+
+app.controller('adddataController', [
+  '$scope', '$element', 'title', 'close', 
+  function($scope, $element, title, close) {
+
+  $scope.name = null;
+  $scope.title = title;
+  
+  //  This close function doesn't need to use jQuery or bootstrap, because
+  //  the button has the 'data-dismiss' attribute.
+  $scope.close = function() {
+ 	  close({
+      name: $scope.name,
+    },0); 
+  };
+
+  //  This cancel function must use the bootstrap, 'modal' function because
+  //  the doesn't have the 'data-dismiss' attribute.
+  $scope.cancel = function() {
+
+    //  Manually hide the modal.
+    $element.modal('hide');
+    
+    //  Now call close, returning control to the caller.
+    close({
+      name: $scope.name,
+    },0); 
+  };
+
+}]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,64 @@
+'use strict';
+
+
+/**
+ * @ngdoc function
+ * @name mytableApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mytableApp
+ */
+
+angular.module('mytableApp')
+.controller('MainCtrl', function($scope) {
+    
+	$scope.employees= [
+    { 'name':'EMP NAME 1',
+			
+       'designation':
+'Director'
+,
+			       'dateOfJoining':'12-12-2016T00:00:00',
+			       'email':'emp1@company.com',
+			       'skills':'Management,Process Training,Recruitment',
+			       'status':'active',
+			       'remarks':'He is one of the founders of the company' 
+			     },
+			     { 'name':'EMP NAME 2',
+			
+       'designation':
+'Developer'
+,
+			       'dateOfJoining':'12-12-2016T00:00:00',
+			       'email':'emp2@company.com',
+			       'skills':'Java,HTML,CSS',
+			       'status':'left',
+			       'remarks':'Terminated due to copyright infringement' 
+			     },];
+
+	$scope.addRow = function(){		
+	$scope.employees.push({ 'name':$scope.name, 'designation': $scope.designation, 'email':$scope.email, 'skills': $scope.skills, 'status':$scope.status, 'remarks': $scope.remarks });
+	$scope.name='';
+	$scope.designation='';
+	$scope.email='';
+	$scope.skills='';
+	$scope.status='';
+	$scope.remarks='';
+	};
+
+	$scope.removeRow=function(name){
+	var index=-1;
+	var comArr=eval($scope.employees);
+	for(var i=0; i < comArr.length; i++){
+		if(comArr[i].name === name){
+			index=i;
+			break;
+		}
+	}
+	
+	$scope.employees.splice(index,1);
+	};
+	
+
+
+});
